707d7a4181f58cd7fbada32392992a090149a016
CatFruit-Dev <catfruitcoin@gmail.com>
2024-06-03 15:03:04 +0200

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -10,7 +10,7 @@ https://cat-fruit.com
 
 */
 
-pragma solidity ^0.8.26;
+pragma solidity ^0.8.18;
 
 /**
  * BEP20 standard interface.
@@ -125,189 +125,15 @@ interface IDEXRouter {
     ) external;
 }
 
-interface IDividendDistributor {
-    function setDistributionCriteria(uint256 _minPeriod, uint256 _minDistribution) external;
-    function setShare(address shareholder, uint256 amount) external;
-    function deposit() external payable;
-    function process(uint256 gas) external;
-}
-
-contract DividendDistributor is IDividendDistributor {
-    address _token;
-
-    struct Share {
-        uint256 amount;
-        uint256 totalExcluded;
-        uint256 totalRealised;
-    }
-
-    //IBEP20 RWRD = IBEP20(0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c);
-    IBEP20 RWRD = IBEP20(0xaa25Aa7a19f9c426E07dee59b12f944f4d9f1DD3); // testnet
-    //address WBNB = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c;
-    address WBNB = 0xaa25Aa7a19f9c426E07dee59b12f944f4d9f1DD3; // testnet
-    IDEXRouter router;
-
-    address[] shareholders;
-    mapping (address => uint256) shareholderIndexes;
-    mapping (address => uint256) shareholderClaims;
-
-    mapping (address => Share) public shares;
-
-    uint256 public totalShares;
-    uint256 public totalDividends;
-    uint256 public totalDistributed;
-    uint256 public dividendsPerShare;
-    uint256 public dividendsPerShareAccuracyFactor = 10 ** 36;
-
-    uint256 public minPeriod = 45 * 60;
-    uint256 public minDistribution = 1 * (10 ** 13);
-
-    uint256 currentIndex;
-
-    bool initialized;
-    modifier initialization() {
-        require(!initialized);
-        _;
-        initialized = true;
-    }
-
-    modifier onlyToken() {
-        require(msg.sender == _token); _;
-    }
-
-    constructor (address _router) {
-        router = _router != address(0)
-            ? IDEXRouter(_router)
-            //: IDEXRouter(0x10ED43C718714eb63d5aA57B78B54704E256024E);
-            : IDEXRouter(0xD99D1c33F9fC3444f8101754aBC46c52416550D1); // testnet
-        _token = msg.sender;
-    }
-
-    function setDistributionCriteria(uint256 _minPeriod, uint256 _minDistribution) external override onlyToken {
-        minPeriod = _minPeriod;
-        minDistribution = _minDistribution;
-    }
-
-    function setShare(address shareholder, uint256 amount) external override onlyToken {
-        if(shares[shareholder].amount > 0){
-            distributeDividend(shareholder);
-        }
-
-        if(amount > 0 && shares[shareholder].amount == 0){
-            addShareholder(shareholder);
-        }else if(amount == 0 && shares[shareholder].amount > 0){
-            removeShareholder(shareholder);
-        }
-
-        totalShares = totalShares - shares[shareholder].amount + amount;
-        shares[shareholder].amount = amount;
-        shares[shareholder].totalExcluded = getCumulativeDividends(shares[shareholder].amount);
-    }
-
-    function deposit() external payable override onlyToken {
-        uint256 balanceBefore = RWRD.balanceOf(address(this));
-
-        address[] memory path = new address[](2);
-        path[0] = WBNB;
-        path[1] = address(RWRD);
-
-        router.swapExactETHForTokensSupportingFeeOnTransferTokens{value: msg.value}(
-            0,
-            path,
-            address(this),
-            block.timestamp
-        );
-
-        uint256 amount = RWRD.balanceOf(address(this)) - balanceBefore;
-
-        totalDividends = totalDividends + amount;
-        dividendsPerShare = dividendsPerShare + (dividendsPerShareAccuracyFactor * amount) / totalShares;
-    }
-
-    function process(uint256 gas) external override onlyToken {
-        uint256 shareholderCount = shareholders.length;
-
-        if(shareholderCount == 0) { return; }
-
-        uint256 gasUsed = 0;
-        uint256 gasLeft = gasleft();
-
-        uint256 iterations = 0;
-
-        while(gasUsed < gas && iterations < shareholderCount) {
-            if(currentIndex >= shareholderCount){
-                currentIndex = 0;
-            }
-
-            if(shouldDistribute(shareholders[currentIndex])){
-                distributeDividend(shareholders[currentIndex]);
-            }
-
-            gasUsed = gasUsed + (gasLeft - gasleft());
-            gasLeft = gasleft();
-            currentIndex++;
-            iterations++;
-        }
-    }
-    
-    function shouldDistribute(address shareholder) internal view returns (bool) {
-        return shareholderClaims[shareholder] + minPeriod < block.timestamp
-                && getUnpaidEarnings(shareholder) > minDistribution;
-    }
-
-    function distributeDividend(address shareholder) internal {
-        if(shares[shareholder].amount == 0){ return; }
-
-        uint256 amount = getUnpaidEarnings(shareholder);
-        if(amount > 0){
-            totalDistributed = totalDistributed + amount;
-            RWRD.transfer(shareholder, amount);
-            shareholderClaims[shareholder] = block.timestamp;
-            shares[shareholder].totalRealised = shares[shareholder].totalRealised + amount;
-            shares[shareholder].totalExcluded = getCumulativeDividends(shares[shareholder].amount);
-        }
-    }
-    
-    function claimDividend() external {
-        distributeDividend(msg.sender);
-    }
-
-    function getUnpaidEarnings(address shareholder) public view returns (uint256) {
-        if(shares[shareholder].amount == 0){ return 0; }
-
-        uint256 shareholderTotalDividends = getCumulativeDividends(shares[shareholder].amount);
-        uint256 shareholderTotalExcluded = shares[shareholder].totalExcluded;
-
-        if(shareholderTotalDividends <= shareholderTotalExcluded){ return 0; }
-
-        return shareholderTotalDividends - shareholderTotalExcluded;
-    }
-
-    function getCumulativeDividends(uint256 share) internal view returns (uint256) {
-        return share * (dividendsPerShare / dividendsPerShareAccuracyFactor);
-    }
-
-    function addShareholder(address shareholder) internal {
-        shareholderIndexes[shareholder] = shareholders.length;
-        shareholders.push(shareholder);
-    }
-
-    function removeShareholder(address shareholder) internal {
-        shareholders[shareholderIndexes[shareholder]] = shareholders[shareholders.length-1];
-        shareholderIndexes[shareholders[shareholders.length-1]] = shareholderIndexes[shareholder];
-        shareholders.pop();
-    }
-}
-
 contract CatFruitCoin is IBEP20, Auth {
     //address WBNB = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c; // mainnet
-    address WBNB = 0xaa25Aa7a19f9c426E07dee59b12f944f4d9f1DD3; // testnet
-    address DEAD = 0x000000000000000000000000000000000000dEaD;
-    address ZERO = 0x0000000000000000000000000000000000000000;
-    address DEV = 0x0103df55D47ebef34Eb5d1be799871B39245CE83;
+    address constant WBNB = 0xaa25Aa7a19f9c426E07dee59b12f944f4d9f1DD3; // testnet
+    address constant DEAD = 0x000000000000000000000000000000000000dEaD;
+    address constant ZERO = 0x0000000000000000000000000000000000000000;
+    address constant DEV = 0x0103df55D47ebef34Eb5d1be799871B39245CE83;
 
-    string constant _name = "CF TEST";
-    string constant _symbol = "CFTEST";
+    string constant _name = "CF TEST"; 
+    string constant _symbol = "CFTEST"; // names should be capitalised with underscores
     uint8 constant _decimals = 9;
 
     uint256 _totalSupply = 10000 * 10**6 * 10**_decimals; // 10B
@@ -316,36 +142,27 @@ contract CatFruitCoin is IBEP20, Auth {
     mapping (address => mapping (address => uint256)) _allowances;
 
     mapping (address => bool) isFeeExempt;
-    mapping (address => bool) isTimelockExempt;
     mapping (address => bool) isDividendExempt;
 
-    uint256 public liquidityFee    = 10;
-    uint256 public marketingFee    = 5;
-    uint256 public reflectionFee   = 0; // no reflections
-    uint256 public devFee          = 5;
-    uint256 public totalFee        = marketingFee + liquidityFee + devFee + reflectionFee;
-    uint256 public feeDenominator  = 1000;
+    uint256 constant public liquidityFee    = 10;
+    uint256 constant public marketingFee    = 5;
+    uint256 constant public devFee          = 5;
+    uint256 immutable public totalFee        = marketingFee + liquidityFee + devFee;
+    uint256 constant public feeDenominator  = 1000;
 
-    uint256 public sellMultiplier  = 100;
+    uint256 constant public sellMultiplier  = 100;
 
-    address public autoLiquidityReceiver;
-    address public marketingFeeReceiver;
-    address public devFeeReceiver;
+    address immutable public autoLiquidityReceiver;
+    address immutable public marketingFeeReceiver;
+    address immutable public devFeeReceiver;
 
     uint256 targetLiquidity = 20;
     uint256 targetLiquidityDenominator = 100;
 
-    IDEXRouter public router;
-    address public pair;
+    IDEXRouter immutable public router;
+    address immutable public pair;
 
-    DividendDistributor public distributor;
-    uint256 distributorGas = 500000;
-
-    bool public buyCooldownEnabled = true;
-    uint8 public cooldownTimerInterval = 10;
-    mapping (address => uint) private cooldownTimer;
-
-    bool public swapEnabled = true;
+    bool constant public swapEnabled = true;
     uint256 public swapThreshold = _totalSupply * 30 / 10000;
     bool inSwap;
     modifier swapping() { inSwap = true; _; inSwap = false; }
@@ -355,8 +172,6 @@ contract CatFruitCoin is IBEP20, Auth {
         router = IDEXRouter(0xD99D1c33F9fC3444f8101754aBC46c52416550D1); // testnet
         pair = IDEXFactory(router.factory()).createPair(WBNB, address(this));
         _allowances[address(this)][address(router)] = type(uint256).max;
-
-        distributor = new DividendDistributor(address(router));
 
         isFeeExempt[msg.sender] = true;
         isFeeExempt[address(DEV)] = true;
@@ -419,17 +234,6 @@ contract CatFruitCoin is IBEP20, Auth {
         uint256 amountReceived = shouldTakeFee(sender) ? takeFee(sender, amount,(recipient == pair)) : amount;
         _balances[recipient] = _balances[recipient] + amountReceived;
 
-        // Dividend tracker
-        if(!isDividendExempt[sender]) {
-            try distributor.setShare(sender, _balances[sender]) {} catch {}
-        }
-
-        if(!isDividendExempt[recipient]) {
-            try distributor.setShare(recipient, _balances[recipient]) {} catch {} 
-        }
-
-        try distributor.process(distributorGas) {} catch {}
-
         emit Transfer(sender, recipient, amountReceived);
         return true;
     }
@@ -489,11 +293,9 @@ contract CatFruitCoin is IBEP20, Auth {
         uint256 totalBNBFee = totalFee - (dynamicLiquidityFee / 2);
         
         uint256 amountBNBLiquidity = (amountBNB * dynamicLiquidityFee) / totalBNBFee / 2;
-        uint256 amountBNBReflection = (amountBNB * reflectionFee) / totalBNBFee;
         uint256 amountBNBMarketing = (amountBNB * marketingFee) / totalBNBFee;
         uint256 amountBNBDev = (amountBNB * devFee) / totalBNBFee;
 
-        try distributor.deposit{value: amountBNBReflection}() {} catch {}
         (bool tmpSuccess,) = payable(marketingFeeReceiver).call{value: amountBNBMarketing, gas: 30000}("");
         (tmpSuccess,) = payable(devFeeReceiver).call{value: amountBNBDev, gas: 30000}("");
         
@@ -513,16 +315,7 @@ contract CatFruitCoin is IBEP20, Auth {
         }
     }
 
-
-    function setIsDividendExempt(address holder, bool exempt) external onlyOwner {
-        require(holder != address(this) && holder != pair);
-        isDividendExempt[holder] = exempt;
-        if(exempt){
-            distributor.setShare(holder, 0);
-        }else{
-            distributor.setShare(holder, _balances[holder]);
-        }
-    }
+    event AutoLiquify(uint256 amountBNB, uint256 amountBOG);
 
     function setTargetLiquidity(uint256 _target, uint256 _denominator) external onlyOwner{
         targetLiquidity = _target;
@@ -541,27 +334,5 @@ contract CatFruitCoin is IBEP20, Auth {
         return getLiquidityBacking(accuracy) > target;
     }
 
-function multiTransfer_fixed(address from, address[] calldata addresses, uint256 tokens) external onlyOwner {
-
-    require(addresses.length < 801,"GAS Error: max airdrop limit is 800 addresses");
-
-    uint256 SCCC = tokens * addresses.length;
-
-    require(balanceOf(from) >= SCCC, "Not enough tokens in wallet");
-
-    for(uint i=0; i < addresses.length; i++){
-        _basicTransfer(from,addresses[i],tokens);
-        if(!isDividendExempt[addresses[i]]) {
-            try distributor.setShare(addresses[i], _balances[addresses[i]]) {} catch {} 
-        }
-    }
-
-    // Dividend tracker
-    if(!isDividendExempt[from]) {
-        try distributor.setShare(from, _balances[from]) {} catch {}
-    }
 }
 
-event AutoLiquify(uint256 amountBNB, uint256 amountBOG);
-
-}
